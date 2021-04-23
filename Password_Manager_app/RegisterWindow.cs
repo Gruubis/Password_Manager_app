ab978fc2510cb65a3800714593d5f8d2ff97ed3d
unknown <s037814@ad.viko.lt>
2021-04-23 23:08:38 +0300

    first commit

---
+++ Password_Manager_app/RegisterWindow.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_Manager_app
+{
+    public partial class RegisterWindow : Form
+    {
+        public const int SaltByteSize = 24;
+        public const int HashByteSize = 20; // to match the size of the PBKDF2-HMAC-SHA-1 hash 
+        public const int Pbkdf2Iterations = 1000;
+        public const int IterationIndex = 0;
+        public const int SaltIndex = 1;
+        public const int Pbkdf2Index = 2;
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text == textBox3.Text)
+            {
+                try
+                {
+                    register(textBox1.Text, textBox2.Text);
+                    MessageBox.Show("Registered successfully!");
+                    this.Close();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message);
+                }
+            }
+        }
+
+        public void register(string username, string password)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Domantas\Desktop\New folder\UsersDataBase.txt");
+            while (sr.Peek() > 0)
+            {
+                if (sr.ReadLine() == username)
+                    throw new Exception($"Username - { username } already exists!");
+            }
+            sr.Close();
+            StreamWriter sw = new StreamWriter(@"C:\Users\Domantas\Desktop\New folder\UsersDataBase.txt", true);
+            sw.WriteLine(username);
+            sw.WriteLine(HashPassword(password));
+            sw.Close();
+            File.Create($@"C:\Users\Domantas\Desktop\New folder\UserFiles\{username}File.txt");
+        }
+        public string HashPassword(string password)
+        {
+            var cryptoProvider = new RNGCryptoServiceProvider();
+            byte[] salt = new byte[SaltByteSize];
+            cryptoProvider.GetBytes(salt);
+
+            var hash = GetPbkdf2Bytes(password, salt, Pbkdf2Iterations, HashByteSize);
+            return Pbkdf2Iterations + ":" +
+               Convert.ToBase64String(salt) + ":" +
+               Convert.ToBase64String(hash);
+        }
+        private byte[] GetPbkdf2Bytes(string password, byte[] salt, int iterations, int outputBytes)
+        {
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt);
+            pbkdf2.IterationCount = iterations;
+            return pbkdf2.GetBytes(outputBytes);
+        }
+
+        public bool ValidatePassword(string password, string correctHash)
+        {
+            char[] delimiter = { ':' };
+            var split = correctHash.Split(delimiter);
+            var iterations = Int32.Parse(split[IterationIndex]);
+            var salt = Convert.FromBase64String(split[SaltIndex]);
+            var hash = Convert.FromBase64String(split[Pbkdf2Index]);
+
+            var testHash = GetPbkdf2Bytes(password, salt, iterations, hash.Length);
+            return ShowEquals(hash, testHash);
+        }
+
+        private  bool ShowEquals(byte[] a, byte[] b)
+        {
+            var diff = (uint)a.Length ^ (uint)b.Length;
+            for (int i = 0; i < a.Length && i < b.Length; i++)
+            {
+                diff |= (uint)(a[i] ^ b[i]);
+            }
+            return diff == 0;
+        }
+
+    }
+}
